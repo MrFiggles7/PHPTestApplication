2ab1ebca89a46efac93d9adaab9ad1fe6915ee7e
MrFiggles7 <rdjohnson7478@gmail.com>
2022-01-21 17:57:57 -0600

    init commit

---
+++ tpe-vue-php-api_test/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    {{thing}}
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  data() {
+    return{
+      thing: 'this thing'
+    }
+  },
+
+  created() {
+    const axios = require('axios');
+
+    axios.get('http://localhost/PHPTestApplication/api/actor/read.php').then((response)=>{
+      this.thing = response
+    })
+  },
+
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
